cb23ced9a243a9a062606045e6d4302e5f764c49
Y-v-a-i-n <107935034+Y-v-a-i-n@users.noreply.github.com>
2022-11-06 17:43:30 +0100

    ProxyChecker

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ const request = require("request"),
     proxyRegex = /([0-9A-F]{0,4}\:[0-9A-F]{0,4}\:[0-9A-F]{0,4}\:[0-9A-F]{0,4}\:[0-9A-F]{0,4}\:[0-9A-F]{0,4}\:[0-9A-F]{0,4}\:[0-9A-F]{0,4}|([0-9]{1,2}|1[0-9]{1,2}|2[0-5]{2})\.([0-9]{1,2}|1[0-9]{1,2}|2[0-5]{2})\.([0-9]{1,2}|1[0-9]{1,2}|2[0-5]{2})\.([0-9]{1,2}|1[0-9]{1,2}|2[0-5]{2}))\:([0-9]{1,5})/gi,
     check = (list, t) => {
         var types = ["http", "https", "socks4", "socks5"],
-            urls = ["https://www.fbi.gov/", "https://www.nsa.gov/", "https://www.cia.gov/", "https://www.nasa.gov", "https://www.usa.gov/", "https://www.defense.gov/"];
+            urls = ["https://www.fbi.gov/", "https://www.nsa.gov/", "https://www.cia.gov/", "https://www.nasa.gov", "https://www.usa.gov/", "https://www.defense.gov/"],
+            outputA = document.getElementById("outputA"), outputB = document.getElementById("outputB");
         t < 3 && (types = [types[t]]), list.match(proxyRegex) ? types.forEach((type => {
             list.match(proxyRegex).forEach((proxy => {
                 if (Number(proxy.split("").reverse().join("").split(":")[0].split("").reverse().join("")) <= 65535) {
@@ -11,21 +12,24 @@ const request = require("request"),
                     require("throttled-queue")(15, 1e3)(function () {
                         request({url: String(url),method: "GET",proxy: String(`${type}://${proxy}`)}, ((err, res) => {
                             var response = {type: type,proxy: proxy,url: url,country: country,city: city,status: null,valid: null};
-                            err ? (response.status = 500, response.valid = !1, document.getElementById("outputA").value += `${JSON.stringify(response)}\n`) : 200 == res.statusCode ? (response.status = res.statusCode, response.valid = !0, document.getElementById("outputA").value += `${JSON.stringify(response)}\n`, document.getElementById("outputB").value += proxy + "\n") : (response.status = res.statusCode, response.valid = !1, document.getElementById("outputA").value += `${JSON.stringify(response)}\n`)
+                            err ? (response.status = 500, response.valid = !1, outputA.value += `${JSON.stringify(response)}\n`) : 200 == res.statusCode ? (response.status = res.statusCode, response.valid = !0, outputA.value += `${JSON.stringify(response)}\n`, outputB.value += proxy + "\n") : (response.status = res.statusCode, response.valid = !1, outputA.value += `${JSON.stringify(response)}\n`)
+                            outputA.scrollTop = outputA.scrollHeight, inputB.scrollTop = inputB.scrollHeight
                         }))
                     })
-                } else document.getElementById("outputA").value += `${JSON.stringify({type:type,proxy:proxy,error:"Invalid Port."})}\n`
+                } else (outputA.value += `${JSON.stringify({type:type,proxy:proxy,error:"Invalid Port."})}\n`,outputA.scrollTop = outputA.scrollHeight)
             }))
-        })) : document.getElementById("outputA").value = `${JSON.stringify({error:"No valid proxy was found."})}\n`
+        })) : (outputA.value = `${JSON.stringify({error:"No valid proxy was found."})}\n`, outputA.scrollTop = outputA.scrollHeight)
     },
     scrap = list => {
+        inputB = document.getElementById("inputB"), outputA = document.getElementById("outputA");
         list.match(urlRegex) ? list.match(urlRegex).forEach((url => {
             request({url: String(url),method: "GET"}, ((err, res, body) => {
-                if (err) document.getElementById("outputA").value += `${JSON.stringify({url:url,status:500,error:!0,found:0})}\n`;
+                if (err) outputA.value += `${JSON.stringify({url:url,status:500,error:!0,found:0})}\n`;
                 else if (body && body.match(proxyRegex)) {
                     const proxys = body.match(proxyRegex);
-                    document.getElementById("inputB").value += proxys.join("\n"), document.getElementById("outputA").value += `${JSON.stringify({url:url,status:res.statusCode,error:!1,found:proxys.length})}\n`
+                    inputB.value += proxys.join("\n"), outputA.value += `${JSON.stringify({url:url,status:res.statusCode,error:!1,found:proxys.length})}\n`
                 }
+                outputA.scrollTop = outputA.scrollHeight, inputB.scrollTop = inputB.scrollHeight
             }))
-        })) : document.getElementById("outputA").value = `${JSON.stringify({error:"No valid proxy was found."})}\n`
+        })) : (outputA.value = `${JSON.stringify({error:"No valid proxy was found."})}\n`,outputA.scrollTop = outputA.scrollHeight)
     };
